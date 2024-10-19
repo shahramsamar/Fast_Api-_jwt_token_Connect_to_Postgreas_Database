75d890c386820883eb7eb042de364530a805633f
shahramsamar <shahramsamar2010@gmail.com>
2024-10-20 00:26:52 +0330

    connected to data base posrgress

--- database/config.py
+++ database/config.py
@@ -3,7 +3,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./sqlite.db"
+    DATABASE_URL: str = "postgresql://postgres:0000@127.0.0.1:5432/blog"
+
     
     SECRET_KEY: str = "change me"
     ACCESS_EXPIRATION: int = 60  # 1 hour
